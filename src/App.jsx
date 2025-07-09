43e6eae8880ab8e7c0bd30d940cd306c908a5ab3
Nitin-Lohumi <lohuminitin@gmail.com>
2025-07-09 17:13:43 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -47,10 +47,10 @@ export default function App() {
       setTimeout(() => {
         setError(
           <i>
-            In a motor accident claim where the deceased was self-employed and
+            use this -  " In a motor accident claim where the deceased was self-employed and
             aged 54–55 years at the time of death, is the claimant entitled to
             an addition towards future prospects in computing compensation under
-            Section 166 of the Motor Vehicles Act, 1988? If so, how much?
+            Section 166 of the Motor Vehicles Act, 1988? If so, how much? "
           </i>
         );
       }, 1500);
